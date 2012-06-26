75c1f44186c8bd832c8156a8094834897710c0f2
xTristan <tristan.uva@gmail.com>
2012-06-26 00:54:57 -0700

    add models for person and relationships.

--- histo/characters/models.py
+++ histo/characters/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=128)
+    birth_date = models.DateTimeField(blank='true', null='true')
+    death_date = models.DateTimeField(blank='true', null='true')
+    nationality = models.CharField(max_length=128, blank='true', null='true')
+    description = models.TextField()
+    
+    def __unicode__(self):
+        s = self.name;
+        if self.birth_date:
+            s = s.join(", born on:", self.birth_date)
+        if self.nationality:
+            s = s.join(" in ", self.nationality)
+        if self.death_date:
+            s = s.join(", died on:", self.death_date)
+        s.join("\n", self.description)
+        
+        return s;
+    
+class Relationship(models.Model):
+    relationship_subject = models.ForeignKey(Person, related_name='relationship_subject')
+    relationship_object = models.ForeignKey(Person, related_name='relationship_object')
+    is_bi_directional = models.BooleanField(default='true', blank='true')
+    relationship = models.TextField()
--- histo/histo/settings.py
+++ histo/histo/settings.py
@@ -11,12 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'myHisto',                                         # Or path to database file if using sqlite3.
+        'USER': 'postgres',                                 # Not used with sqlite3.
+        'PASSWORD': 'XuanFeng',                             # Not used with sqlite3.
+        'HOST': 'localhost',                                # Set to empty string for localhost. (This is a fxxking lie for postgres.) Not used with sqlite3.
+        'PORT': '5432',                                     # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -27,7 +27,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -118,8 +118,10 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'characters',
+    'timeline',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
